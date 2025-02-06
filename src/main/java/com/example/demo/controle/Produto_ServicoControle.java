048117e45de23498f0ee14d97ff521b84f3ac789
George Gonçalves <georgeggmiranda@gmail.com>
2025-02-06 15:27:03 -0300

    Criado método retornar por id Produto_Servico.

--- src/main/java/com/example/demo/controle/Produto_ServicoControle.java
+++ src/main/java/com/example/demo/controle/Produto_ServicoControle.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,12 @@ public class Produto_ServicoControle {
 		List<Produto_ServicoDTO> dto = lista.stream().map(
 				x -> new Produto_ServicoDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(dto);
-	}	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto_ServicoDTO> encontrarPorId(
+			@PathVariable int id) {
+		Produto_Servico obj = psServico.encontrarPorId(id);
+		return ResponseEntity.ok().body(new Produto_ServicoDTO(obj));
+	}
 }
--- src/main/java/com/example/demo/servico/Produto_ServicoServico.java
+++ src/main/java/com/example/demo/servico/Produto_ServicoServico.java
@@ -1,12 +1,14 @@
 package com.example.demo.servico;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entidades.Produto_Servico;
 import com.example.demo.repositorio.Produto_ServicoRepositorio;
+import com.example.demo.servico.excecao.ObjetoNaoEncontrado;
 
 @Service
 public class Produto_ServicoServico {
@@ -17,4 +19,10 @@ public class Produto_ServicoServico {
 	public List<Produto_Servico> encontrarTodos() {
 		return psRepositorio.findAll();
 	}
+	
+	public Produto_Servico encontrarPorId(int id) {
+		Optional<Produto_Servico> usuario = psRepositorio.findById(id);
+		return usuario.orElseThrow(()-> new ObjetoNaoEncontrado(
+				"Objeto não encontrado"));
+	}	
 }
